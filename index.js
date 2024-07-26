e81f4623e5999928dd56f22d1bc0ea9f27e5bae5
TheJigmeYeshey <140654738+TheJigmeYeshey@users.noreply.github.com>
2024-07-26 15:18:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("I caused a merge conflict again")
+function addTwoNumbers(a, b) {
+    return a + b;
+}
 
+const sum = addTwoNumbers(5, 10);
+console.log("The sum of 5 and 10 is: " + sum)
